4c1cb0aa4db164ce579c11509a9543cb01551028
maugares <mauro.garciaesteban@gmail.com>
2019-04-02 17:32:12 +0200

    add list of products in the component CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './CartItem.css'
 
 export default class CartItem extends Component {
 
+  products = [
+    {
+      id: 1,
+      name: "bread",
+      price: 1.20,        // 1.20 €/loaf
+    },
+    {
+      id: 2,
+      name: "ham",
+      price: 7.00,        // 7.00 €/Kg
+    },
+    {
+      id: 3,
+      name: "cheese",
+      price: 8.90,        // 8.90 €/Kg
+    },
+    {
+      id: 4,
+      name: "beer",
+      price: 1.20,        // 1.20 €/L
+    }
+  ]
+
   render() {
     return <div>
-      <li className="cart-items">Items</li>
+      <li className="cart-items">
+        Items:
+        <ul className="items">
+        </ul>
+      </li>
     </div>
   }
 }
