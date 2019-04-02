deec7e62827eb7863424bb38eed92891523a8cdf
maugares <mauro.garciaesteban@gmail.com>
2019-04-02 17:29:41 +0200

    Add components CheckoutButton and CartItems

---
+++ src/components/CartItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './CartItem.css'
+
+export default class CartItem extends Component {
+
+  render() {
+    return <div>
+      <li className="cart-items">Items</li>
+    </div>
+  }
+}
---
+++ src/components/CheckoutButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './CheckoutButton.css'
+
+export default class CheckoutButton extends Component {
+
+  render() {
+    return <div>
+      <button className="checkout-button">Button</button>
+    </div>
+
+    
+  }
+}
